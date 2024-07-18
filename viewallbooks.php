cf632a332c78b9f628e2d8476e00b0ccf6dcfbc8
em-au <146881865+em-au@users.noreply.github.com>
2024-07-17 22:18:09 -0400

    Change button dropdown for book options

--- viewallbooks.php
+++ viewallbooks.php
@@ -26,27 +26,24 @@
                         <div class="card-body">
                             <h5 class="card-title"> <?php echo $row['title'] ?></h5>
                             <p class="card-text">
-                                Author: <?php echo $row['author'] ?></p>
-                            <!-- <i><?php echo $row['status'] ?></i> -->
-
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-outline-secondary"><i><?php echo $row['status'] ?></i></button>
-                                <button type="button" class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" 
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                        <span class="visually-hidden">Toggle Dropdown</span>
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="marktoread.php?id=
-                                        <?php echo $row['id']?>">To read</a></li>
-                                    <li><a class="dropdown-item" href="markcurrentlyreading.php?id=
-                                        <?php echo $row['id']?>">Currently reading</a></li>
-                                    <li><a class="dropdown-item" href="markread?id=
-                                        <?php echo $row['id']?>">Read</a></li>
-                                </ul>
+                                <?php echo $row['author'] ?></p>
+                            <div class="d-flex justify-content-between">
+                                <div class="dropdown">
+                                    <button class="btn btn-outline-secondary dropdown-toggle" 
+                                        type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <?php echo $row['status'] ?></button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="marktoread.php?id=
+                                            <?php echo $row['id']?>">To read</a></li>
+                                        <li><a class="dropdown-item" href="markcurrentlyreading.php?id=
+                                            <?php echo $row['id']?>">Currently reading</a></li>
+                                        <li><a class="dropdown-item" href="markread.php?id=
+                                            <?php echo $row['id']?>">Read</a></li>
+                                    </ul>
+                                </div>
+                                <a href="deletebook.php?id=<?php echo $row['id']?>" class="btn btn-outline-secondary">
+                                    <i class="fa-solid fa-trash" style="color: #a09abc;"></i></a>
                             </div>
-
-                            <div><a href="deletebook.php?id=<?php echo $row['id']?>" class="btn btn-outline-secondary">
-                                <i class="fa-solid fa-trash" style="color: #a09abc;"></i></a></div>
                         </div>
                     </div>
                 </div>
