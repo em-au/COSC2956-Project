02dba98490ef6e60c7566b079758355cbfa3450e
em-au <146881865+em-au@users.noreply.github.com>
2024-07-18 20:30:11 -0400

    Edit layout of Home for logged in users

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
     $title = "Home";
     require_once 'includes/header.php';
+    require_once 'db/conn.php';
     session_start();
 ?>
 
@@ -14,14 +15,45 @@
                     Get started now</button></a>
             </div>
             <div class="banner-image">
-                <div><img src="pics/reading-book.png"></div>
+                <div><img src="pics/reading-book.png" alt="An illustration of a woman reading a book"></div>
                 <div class="image-source"><a href="https://www.flaticon.com/free-stickers/student" title="student stickers">Student stickers created by Stickers - Flaticon</a></div>
-            </div> <!-- ADD IMAGE ATTRIBUTION 
-            <a href="https://www.flaticon.com/free-stickers/student" title="student stickers">Student stickers created by Stickers - Flaticon</a>-->
+            </div>
         </div>
     <?php }
     else if (isset($_SESSION['auth'])) { ?>
-        <h1>Hello, <?php echo $_SESSION['username'] . "!" ?></h1>
+        <div class="container home-banner-white">
+            <div class="banner-image">
+                <div><img src="pics/books.png" class="books-icon" alt="An illustration of books on a shelf"></div>
+                <div class="image-source"><a href="https://www.flaticon.com/free-stickers/reading" title="reading stickers">Reading stickers created by Stickers - Flaticon</a></div>
+            </div>
+            <div class="banner-text">
+                <h2>CURRENTLY READING</h2>
+                <?php 
+                    // SQL statement to retrieve read books for a user
+                    $user_id = $_SESSION['user_id'];
+                    $sql = "SELECT * FROM books WHERE user_id = $user_id AND status = 'Currently reading'";
+
+                    // Execute the SQL statement and get results
+                    $result = mysqli_query($conn, $sql); // $result contains either the result set object or false
+                    if ($result) { 
+                        if (mysqli_num_rows($result) == 0) {
+                            echo "<i>You have no books marked as 'currently reading'</i><br><br>";
+                        }
+                    }
+                    while ($row = mysqli_fetch_assoc($result)) { ?> 
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <?php echo "<b>" . $row['title'] . "</b> by " . $row['author']; ?>
+                            </div>
+                        </div>
+                    <?php }
+                ?>
+                <a href="viewallbooks.php"><button type="button" class="btn btn-primary btn-lg home-button">
+                    See all books</button></a>
+            </div>
+            
+        </div>
+    </div>
     <?php }
 ?>
 
